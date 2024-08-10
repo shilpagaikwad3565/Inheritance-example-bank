9e20359a5b69380083569fdde243e532eb72e179
shilpa gaikwad <Shilpagaikwad79879@gmail.com>
2024-08-10 11:31:31 +0530

    ICICI.java

---
+++ ICICI.java
@@ -0,0 +1,32 @@
+package bankdemo;
+
+import java.util.Scanner;
+
+public class ICICI extends Bank {
+  
+	   private double getCalculateintereset(double principleamount,int tenure,double rateOfinterest)
+	{   
+		   double sbiinterest=(principleamount*tenure*rateOfinterest)/100;
+			System.out.println("SBI interest is:"+sbiinterest);
+		
+		 		return 0;
+	}
+	 public void getICICBankDetails()
+	{
+		 Scanner sc=new Scanner(System.in);
+			System.out.println("Enter principleamount:");
+		principleamount =sc.nextDouble();
+			System.out.println("Enter tenure:");
+			  tenure=sc.nextInt();
+			System.out.println("enter rateOfinterest:");
+			  rateOfinterest=sc.nextDouble();
+		
+					 System.out.println("ICICI bank details are");
+			  System.out.println("principleamount is:"+ principleamount);
+				System.out.println("tenure is:"+tenure);
+				System.out.println("rateOfinterest is:"+ rateOfinterest);
+				getCalculateintereset(principleamount,tenure,rateOfinterest);
+				
+				
+	}
+}
